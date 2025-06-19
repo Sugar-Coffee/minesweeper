2b32da7095486714cc02e5bbbd1ebb312e572795
Cookizza <josh@digitalcake.co.nz>
2025-06-19 17:25:02 +1200

    NO-TICKET - Fix sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,12 +1,14 @@
 import { MetadataRoute } from 'next'
 
+export const dynamic = 'force-static'
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://minesweeper.sugarandcoffee.co.uk'
 
   return [
     {
       url: baseUrl,
-      lastModified: new Date(),
+      lastModified: new Date('2024-01-01'),
       changeFrequency: 'weekly',
       priority: 1,
     },
